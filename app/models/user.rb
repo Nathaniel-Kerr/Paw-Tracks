744617ba195bd7bc496502b335effd11890f6af5
Nathaniel <NathanielKerr10@gmail.com>
2021-04-20 15:41:56 -0700

    refactored user.rb, deleted spec files

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
     has_secure_password
     validates_uniqueness_of :email
-    validates_presence_of :email
-    validates_presence_of :name
+    validates_presence_of :email, :name
+    
     has_many :pets
 end
